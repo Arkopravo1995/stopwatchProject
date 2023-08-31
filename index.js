1f774cba9ee4c9e1a80c30c7f0a6917bc8bc908e
arkopravo <arka.bhuin@gmail.com>
2023-08-31 17:16:15 +0530

    files upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let hr = 0;
+let min = 0;
+let sec = 0;
+let count = 0;
+
+
+let timer =false;
+
+function start(){
+  timer = true;
+  stopwatch();
+}
+
+function stop(){
+    timer = false;
+
+}
+
+function reset(){
+    timer = false;
+    hr = 0;
+    min = 0;
+    sec = 0;
+    count = 0;
+    document.getElementById("hr").innerHTML="00";
+    document.getElementById("min").innerHTML="00";
+    document.getElementById("sec").innerHTML="00";
+    document.getElementById("count").innerHTML="00";
+    
+
+}
+function stopwatch(){
+    if(timer == true){
+        count = count+1;
+
+        if(count == 100){
+            sec =sec + 1;
+            count = 0;
+        }
+        if(sec == 60){
+            min = min+1;
+            sec = 0;
+        }
+        if(min == 60){
+            hr = hr+1;
+            min = 0 ;
+            sec = 0;
+        }
+        var hrStr =hr;
+        var minStr =min;
+        var secStr = sec;
+        var countStr = count;
+        if(hr < 10){
+            hrStr ="0"+hrStr;
+        }
+        if(min < 10) {
+            minStr ="0"+minStr;
+        }
+        if(sec < 10){
+            secStr ="0"+secStr;
+
+        }
+        if(count < 10){
+            countStr ="0"+countStr;
+        }
+        document.getElementById("hr").innerHTML = hr;
+        document.getElementById("min").innerHTML = min;
+        document.getElementById("sec").innerHTML = sec;
+        document.getElementById("count").innerHTML = count;
+        setTimeout("stopwatch()",10);
+    }
+
+}
